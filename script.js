2cd5c19498a3801f510bb0c476a86f4d7e260f06
Christelle CHARLES-ALFRED <christellecharlesalfred56@gmail.com>
2024-11-06 18:51:51 +0100

    projet fini

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const imgSlider = document.querySelector('.img-slider');
 const imgFruits = document.querySelectorAll('.img-item.fruit');
+const infoBox = document.querySelector('.info-box');
+const infoSlider = document.querySelector('.info-slider');
+const bgs = document.querySelectorAll('.bg');
 
 const nextBtn = document.querySelector('.next-btn');
 const prevBtn = document.querySelector('.prev-btn');
 
 let indexSlider = 0;
 let index = 0;
+let direction;
 
 nextBtn.addEventListener('click', () => {
     indexSlider++;
@@ -18,6 +22,18 @@ nextBtn.addEventListener('click', () => {
 
     document.querySelector('.fruit.active').classList.remove('active');
     imgFruits[index].classList.add('active');
+
+    document.querySelector('.bg.active').classList.remove('active');
+    bgs[index].classList.add('active');
+
+    if (direction == 1) {
+        infoSlider.prepend(infoSlider.lastElementChild);
+    }
+
+    direction = -1;
+
+    infoBox.style.justifyContent = 'flex-start';
+    infoSlider.style.transform = 'translateY(-25%)';
 });
 
 prevBtn.addEventListener('click', () => {
@@ -31,4 +47,31 @@ prevBtn.addEventListener('click', () => {
 
     document.querySelector('.fruit.active').classList.remove('active');
     imgFruits[index].classList.add('active');
+
+    document.querySelector('.bg.active').classList.remove('active');
+    bgs[index].classList.add('active');
+
+    if (direction == -1) {
+        infoSlider.appendChild(infoSlider.firstElementChild);
+    }
+
+    direction = 1;
+
+    infoBox.style.justifyContent = 'flex-end';
+    infoSlider.style.transform = 'translateY(25%)';
+});
+
+infoSlider.addEventListener('transitionend', () => {
+    if (direction == -1) {
+        infoSlider.appendChild(infoSlider.firstElementChild);
+    } else if (direction == 1) {
+        infoSlider.prepend(infoSlider.lastElementChild);
+    }
+    infoSlider.style.transition = 'none';
+    infoSlider.style.transform = 'translateY(0)';
+
+    setTimeout(() => {
+        infoSlider.style.transition =
+            '0.5s cubic-bezier(0.645, 0.045, 0.355, 1)';
+    });
 });
